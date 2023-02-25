06bf448a8c2dd8e8d207940b3f7aa7cc8d8dc46a
sweinkauf <scott.weinkauf29@gmail.com>
2023-02-24 22:26:38 -0600

    pages/planning components

---
+++ src/pages/About.js
@@ -0,0 +1,149 @@
+import React from 'react';
+import styled from 'styled-components';
+import PText from '../components/PText';
+import Button from '../components/Button';
+import AboutImg from '../assets/images/about-page-img.png';
+import AboutInfoItem from '../components/AboutInfoItem';
+import ContactBanner from '../components/ContactBanner';
+
+const AboutPageStyles = styled.div`
+  padding: 20rem 0 10rem 0;
+
+  .top-section {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 2rem;
+  }
+  .left {
+    flex: 3;
+  }
+  .right {
+    flex: 2;
+  }
+  .about__subheading {
+    font-size: 2.2rem;
+    margin-bottom: 2rem;
+    span {
+      background-color: var(--deep-dark);
+      padding: 0.5rem;
+      border-radius: 8px;
+    }
+  }
+  .about__heading {
+    font-size: 3.6rem;
+    margin-bottom: 3rem;
+  }
+  .about__info {
+    margin-bottom: 4rem;
+    .para {
+      max-width: 100%;
+    }
+  }
+  .right {
+    img {
+      border: 2px solid var(--gray-1);
+    }
+  }
+  .about__info__items {
+    margin-top: 15rem;
+  }
+  .about__info__item {
+    margin-bottom: 10rem;
+  }
+  .about__info__heading {
+    font-size: 3.6rem;
+    text-transform: uppercase;
+  }
+  @media only screen and (max-width: 768px) {
+    padding: 10rem 0;
+    .top-section {
+      flex-direction: column;
+      gap: 5rem;
+    }
+    .about__subheading {
+      font-size: 1.8rem;
+    }
+    .about__heading {
+      font-size: 2.8rem;
+    }
+    .about__info__heading {
+      font-size: 3rem;
+    }
+  }
+`;
+
+export default function About() {
+  return (
+    <>
+      <AboutPageStyles>
+        <div className="container">
+          <div className="top-section">
+            <div className="left">
+              <p className="about__subheading">
+                Hello, I am <span>Scott Weinkauf</span>
+              </p>
+              <h2 className="about__heading">A freelance Web developer</h2>
+              <div className="about__info">
+                <PText>
+                  I am from Topeka, Kansas. 
+                  <br /> <br />
+                  I started coding in 2022
+                  <br />
+                  <br />
+                  I looking forward to the journey of coding
+                </PText>
+              </div>
+              <Button btnText="Download CV" btnLink="#" />
+            </div>
+            <div className="right">
+              <img src={AboutImg} alt="me" />
+            </div>
+          </div>
+          <div className="about__info__items">
+            <div className="about__info__item">
+              <h1 className="about__info__heading">Education</h1>
+
+              <AboutInfoItem
+                title="High School"
+                items={['Topeka High']}
+              />
+              <AboutInfoItem
+                title="College"
+                items={['Bachelor of Arts from Washburn University']}
+              />
+              <AboutInfoItem
+                title="Full Stack Bootcamp"
+                items={['University Of Kansas']}
+              />
+            </div>
+            <div className="about__info__item">
+              <h1 className="about__info__heading">My Skills</h1>
+
+              <AboutInfoItem
+                title="FrontEnd"
+                items={['HTML', 'CSS', 'JavaScript', 'REACT']}
+              />
+              <AboutInfoItem
+                title="BackEnd"
+                items={['Node', 'Express', 'MySQL']}
+              />
+              <AboutInfoItem
+                title="Design"
+                items={['Adobe Photoshop', 'Adobe Premiere']}
+              />
+            </div>
+            <div className="about__info__item">
+              <h1 className="about__info__heading">Experiences</h1>
+              <AboutInfoItem
+                title="2023-Present"
+                items={['Freelance web Developer']}
+              />
+            </div>
+          </div>
+        </div>
+        <ContactBanner />
+      </AboutPageStyles>
+    </>
+  );
+}
