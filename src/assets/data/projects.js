c842113f6742bba2aeffd128979408d0ac52f6e1
sweinkauf <scott.weinkauf29@gmail.com>
2023-03-09 22:33:46 -0600

    added project

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -1,50 +1,53 @@
-import { v4 as uuidv4 } from 'uuid';
-import CodeNetImg from '../images/CodeNet.png';
-import MovieTrackerImg from '../images/MovieTracker.png';
-import NoteTakerImg from '../images/NoteTaker.png';
-import passwordGenImg from '../images/passwordGen.png';
-import oldPortfolioImg from '../images/oldPortfolio.png';
+import { v4 as uuidv4 } from "uuid";
+import CodeNetImg from "../images/CodeNet.png";
+import MovieTrackerImg from "../images/MovieTracker.png";
+import NoteTakerImg from "../images/NoteTaker.png";
+import passwordGenImg from "../images/passwordGen.png";
+import oldPortfolioImg from "../images/oldPortfolio.png";
+import screensurferImg from "../images/screensurfer.png";
 
 const projects = [
   {
     id: uuidv4(),
-    name: 'Code-Net',
-    link: 'https://protected-wave-25502.herokuapp.com/',
-    desc:
-      'Team Project: Worked with a team to create a small scale social media site geared towards coders. ',
+    name: "Code-Net",
+    link: "https://protected-wave-25502.herokuapp.com/",
+    desc: "Team Project: Worked with a team to create a small scale social media site geared towards coders. ",
     img: CodeNetImg,
   },
   {
     id: uuidv4(),
-    name: 'Movie Traker',
-    link: 'https://angel-pup.github.io/movie-vibes/',
-    desc:
-      'Team Project: Worked with a team to create a site were users can rank their favorite movies. ',
+    name: "Movie Traker",
+    link: "https://angel-pup.github.io/movie-vibes/",
+    desc: "Team Project: Worked with a team to create a site were users can rank their favorite movies. ",
     img: MovieTrackerImg,
   },
   {
     id: uuidv4(),
-    name: 'Note Taker',
-    link: 'https://young-waters-53119.herokuapp.com/',
-    desc:
-      'App that allows user to write and save notes. ',
+    name: "Note Taker",
+    link: "https://young-waters-53119.herokuapp.com/",
+    desc: "App that allows user to write and save notes. ",
     img: NoteTakerImg,
   },
   {
     id: uuidv4(),
-    name: 'Password Generator',
-    link: 'https://sweinkauf.github.io/password-generator/',
-    desc:
-      'App the generates a unique password for the user. ',
+    name: "Password Generator",
+    link: "https://sweinkauf.github.io/password-generator/",
+    desc: "App the generates a unique password for the user. ",
     img: passwordGenImg,
   },
   {
     id: uuidv4(),
-    name: 'Old Portfolio',
-    link: 'https://sweinkauf.github.io/The_Portfolio-of-Scott-Weinkauf/',
-    desc:
-      'See how far I have come. This was my first Portfolio that only used Html and CSS.',
+    name: "Old Portfolio",
+    link: "https://sweinkauf.github.io/The_Portfolio-of-Scott-Weinkauf/",
+    desc: "See how far I have come. This was my first Portfolio that only used Html and CSS.",
     img: oldPortfolioImg,
+  },
+  {
+    id: uuidv4(),
+    name: "Screen Surfer",
+    link: "https://morning-tundra-56106.herokuapp.com/",
+    desc: "MERN app that allows user to save watch list",
+    img: screensurferImg,
   },
 ];
 
